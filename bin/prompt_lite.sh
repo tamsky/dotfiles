54ef1c2d91868c73f5379978939940bd61b9fdf9
Marc Tamsky <tamsky@users.noreply.github.com>
2018-07-27 04:24:00 -0700

    noninteractive please

--- bin/prompt_lite.sh
+++ bin/prompt_lite.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -mi
 STATUS=$(hg prompt "[{status|full}]" 2>/dev/null)
 #STATUS=$( echo $(echo -n $-) )
 echo -n $'\033']0\;${STATUS}$'\007'
 
-BOOKMARK=$( hg prompt "{bookmark}" )
+BOOKMARK=$( hg prompt "{bookmark}" 2>/dev/null )
 screen -X title ${BOOKMARK:-$(basename $SHELL)} 2>/dev/null
