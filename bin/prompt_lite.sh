321ae3dee56b8adbca9aa6db811049f4ea46770d
Marc Tamsky <tamsky@users.noreply.github.com>
2018-07-27 13:34:50 -0700

    async prompt: preserve WINDOW

--- bin/prompt_lite.sh
+++ bin/prompt_lite.sh
@@ -5,4 +5,5 @@ STATUS=$(hg prompt "[{status|full}]" 2>/dev/null)
 echo -n $'\033']0\;${STATUS}$'\007'
 
 BOOKMARK=$( hg prompt "{bookmark}" 2>/dev/null )
-screen -X title ${BOOKMARK:-$(basename $SHELL)} 2>/dev/null
+[[ $BOOKMARK ]] || BOOKMARK="_"
+screen -p $WINDOW -X title "${BOOKMARK}"
