b9e445a5308818aaf288c962fb22245cce4a514a
Marc Tamsky <tamsky@users.noreply.github.com>
2018-07-29 16:26:03 -0700

    screen: enable 2nd line caption embed escapes in prompt_lite.sh window title only = repo name

--- bin/prompt_lite.sh
+++ bin/prompt_lite.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-STATUS=$(hg prompt "[{status|full}]" 2>/dev/null)
-#STATUS=$( echo $(echo -n $-) )
-echo -n $'\033']0\;${STATUS}$'\007'
+# hardstatus text:
 
-BOOKMARK=$( hg prompt "{bookmark}" 2>/dev/null )
+declare -a STATUS=( $(hg prompt "{bookmark}
+[{status|full}]" 2>/dev/null)
+                  )
+
+#STATUS=$( echo $(echo -n $-) )
+
+
+# send window title text to screen via fork/exec:
+REPOROOT=$(hg showconfig bundle.mainreporoot 2>/dev/null)
+REPO=$(basename $REPOROOT 2>/dev/null)
+DIR=$(basename $PWD 2>/dev/null)
+DIRPARENT=$(dirname $(dirname $PWD) 2>/dev/null)
+#BOOKMARK=$( hg prompt "{bookmark}" 2>/dev/null )
+BOOKMARK=${STATUS[0]}
 [[ $BOOKMARK ]] || BOOKMARK="_"
-screen -p $WINDOW -X title "${BOOKMARK}"
+#screen -p $WINDOW -X title "(${REPO}) (${BOOKMARK}) (${DIRPARENT})"
+screen -p $WINDOW -X title "${REPO}"
+
+# emit to SCREEN(1) as a hardstatus string
+echo -n $'\033']0\;${STATUS[1]}"{-} {= wdk}(${BOOKMARK}){-}"$'\007'
