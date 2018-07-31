147b3b9ebc4410d39845b4fa859700a8449affd8
Marc Tamsky <tamsky@users.noreply.github.com>
2018-07-31 01:03:13 -0700

    screen: fancy caption line, async warnings highlighting via AWK(1)

--- bin/prompt_lite.sh
+++ bin/prompt_lite.sh
@@ -2,8 +2,7 @@
 
 # hardstatus text:
 
-declare -a STATUS=( $(hg prompt "{bookmark}
-[{status|full}]" 2>/dev/null)
+declare -a STATUS=( $(hg prompt "[{status|full}]" 2>/dev/null)
                   )
 
 #STATUS=$( echo $(echo -n $-) )
@@ -14,11 +13,30 @@ REPOROOT=$(hg showconfig bundle.mainreporoot 2>/dev/null)
 REPO=$(basename $REPOROOT 2>/dev/null)
 DIR=$(basename $PWD 2>/dev/null)
 DIRPARENT=$(dirname $(dirname $PWD) 2>/dev/null)
-#BOOKMARK=$( hg prompt "{bookmark}" 2>/dev/null )
-BOOKMARK=${STATUS[0]}
-[[ $BOOKMARK ]] || BOOKMARK="_"
-#screen -p $WINDOW -X title "(${REPO}) (${BOOKMARK}) (${DIRPARENT})"
+
+BOOKMARK=$(hg summary 2>/dev/null | awk '/^bookmarks: / { $1="" ;  # delete "bookmarks:" string
+                              gsub("^ ","",$0);        # delete leading spaces
+                              # print warnings
+                              if (NF>1) { multiple_bookmarks=1 ; printf "{! rc}MULTIPLE{-} BOOKMARKS " }
+                              if (index($0,"*")==0) { no_default_bookmark=1 ; printf "{! rc}NO ACTIVE{-} BOOKMARK " }
+                              # gsub = put each bookmark in (parens)
+                              # printf = highlight bookmarks
+                              switch (multiple_bookmarks + no_default_bookmark) {
+                                                   #________________# == highlight stop+space+restart
+                                 case 2 : gsub(" ","){-} {! rc}(",$0) ; printf "{! rc}%s{-}", $0 ; exit 0
+                                 case 1 : gsub(" ","){-} {! bw}(",$0) ; printf "{! bw}(%s){-}", $0 ; exit 0
+                                 case 0 : print $0 ; exit 0
+                              } ;
+                            }'
+           )
+[[ $BOOKMARK ]] || BOOKMARK=$"<"$"<""NO BOOKMARK"$">"$">"
+
 screen -p $WINDOW -X title "${REPO}"
 
 # emit to SCREEN(1) as a hardstatus string
-echo -n $'\033']0\;${STATUS[1]}"{-} {= wdk}(${BOOKMARK}){-}"$'\007'
+#
+#                  --> [AMR!?]-hg flags         --> hg bookmark list
+#                  |                            |
+#                  ~~~~~~~~~~~~                 ~~~~~~~~~~~
+#echo -n $'\033']0\;${STATUS[0]}"{-} {= wdk}(${BOOKMARK}){-}"$'\007'
+echo -n $'\033']0\;${STATUS[0]}"{-} ${BOOKMARK}"$'\007'
